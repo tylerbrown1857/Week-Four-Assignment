b1757266407ade8ede82bfc8c5b92d7f29e0bce4
Daniel Neumann <dkneuma@gmail.com>
2015-09-30 12:10:37 -0400

    Added Piggetty.py

---
+++ piggetty.py
@@ -0,0 +1,34 @@
+#
+# File Header
+#
+# Add your name to this
+
+# Define a function called piggy(string) that returns a string
+
+def piggy(word):
+	
+	# Magic Happens Here
+	pig = word
+	# Ignore previous line
+	
+	return pig
+
+# Open the file *getty.txt* for reading.  
+
+# Open a new file *piggy.txt* for writing.  
+
+# Read the getty.txt file into a string.  
+
+# Strip out bad characters (, - .).  
+
+# Split the string into a list of words.  
+
+# Create a new empty string.  
+
+# Loop through the list of words, pigifying each one.  
+
+# Add the pigified word (and a space) to the new string.  
+
+# Write the new string to piggy.txt.  
+
+# close the files.
